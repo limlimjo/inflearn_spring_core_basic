e4686f4bd28bc4d57ff67242701c1363f736d4bc
selim-jo <skyblue.777@daum.net>
2024-05-11 11:59:47 +0900

    [add] 스프링 핵심 원리 이해1 - 예제 만들기(주문과 할인 도메인 실행과 테스트-실습)

---
+++ 2. 스프링 핵심 원리 이해1 - 예제 만들기/실습/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // given
+        // long memberId(primitive 타입)로 하게 되면 null을 넣을 수 없어 Long 사용
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
