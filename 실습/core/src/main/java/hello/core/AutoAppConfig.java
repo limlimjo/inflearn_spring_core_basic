f9efdc074c50b0f1a2d92851a52e54a2591e272c
selim-jo <skyblue.777@daum.net>
2024-06-05 18:43:54 +0900

    [add] 컴포넌트 스캔 내용 정리(탐색 위치와 기본 스캔 대상)

--- 실습/core/src/main/java/hello/core/AutoAppConfig.java
+++ 실습/core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.FilterType;
 // 컴포넌트 스캔은 이름 그대로 @Component 어노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록함
 @Configuration
 @ComponentScan(
+        // basePackages: 탐색할 패키지의 시작 위치 지정
+        // basePackages를 지정하지 않으면 상단 package에 지정된 거 기준으로 다 뒤짐
+        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
